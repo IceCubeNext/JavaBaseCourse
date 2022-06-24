e9b6567a71692844e6019d9f17de25149342f03b
IceCubeNext <107891941+IceCubeNext@users.noreply.github.com>
2022-06-24 21:51:07 +0300

    Create IsPowerOfTwo.java

---
+++ 2. JavaBasicSyntax/IsPowerOfTwo.java
@@ -0,0 +1,12 @@
+public class IsPowerOfTwo {
+    /**
+     * Checks if given <code>value</code> is a power of two.
+     *
+     * @param value any number
+     * @return <code>true</code> when <code>value</code> is power of two, <code>false</code> otherwise
+     */
+    public static boolean isPowerOfTwo(int value){
+        value = Math.abs(value);
+        return value > 0 && (value & (value - 1)) == 0;
+    }
+}
